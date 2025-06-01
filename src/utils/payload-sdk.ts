ed4168ff19511802b0aebe41d54bb72ec57e3b10
ArseniyDev <jkl@usa.com>
2025-06-01 19:57:21 +0300

    refactor: remove custom fetch implementation from payloadSdk

--- src/utils/payload-sdk.ts
+++ src/utils/payload-sdk.ts
@@ -1,27 +1,7 @@
 import type { Config } from "@/payload-types";
 
 import { PayloadSDK } from "@shopnex/payload-sdk";
-import { cookies } from "next/headers";
 
 export const payloadSdk = new PayloadSDK<Config>({
     baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/api`,
-    fetch: async (input, init) => {
-        const cookieHeader = await cookies();
-        const tenantId = cookieHeader.get("payload-tenant")?.value;
-        const res = await fetch(input, {
-            ...init,
-            headers: {
-                ...init?.headers,
-                "Content-Type": "application/json",
-                "X-Requested-With": "XMLHttpRequest",
-                "x-tenant-id": tenantId || "",
-            },
-        });
-
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
-        }
-
-        return res;
-    },
 });
